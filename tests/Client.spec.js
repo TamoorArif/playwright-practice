0a02cade1096aa8f95bac9503d91bf9d081de0fa
Tamoor <tamoorarif44445@gmail.com>
2025-09-17 15:07:13 +0500

    checkouTess

--- tests/Client.spec.js
+++ tests/Client.spec.js
@@ -1,42 +1,47 @@
-const {test, expect}= require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 
-test('First playwright Test case' , async ({browser})=> {
-//    chorom - Plugins / cookies
-  const context= await browser.newContext();
+test('First playwright Test case', async ({ browser }) => {
+  //    chorom - Plugins / cookies
+  const context = await browser.newContext();
   const page = await context.newPage();
 
-// variable declaration
+  // variable declaration
   const productName = 'ZARA COAT 3';
   const products = page.locator('.card-body')
-  const useremail=page.locator('#userEmail');
-  const userpassword=page.locator("#userPassword");
-  const loginBtn=page.locator('#login');
-  const cardTitles=page.locator('.card-body b');
+  const useremail = page.locator('#userEmail');
+  const userpassword = page.locator("#userPassword");
+  const loginBtn = page.locator('#login');
+  const cardTitles = page.locator('.card-body b');
 
-
-  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-  await useremail.fill('duxaqeq@mailinator.com');
-  await userpassword.fill('Test@12345');
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/");
+  await useremail.fill('zexola@mailinator.com');
+  await userpassword.fill('Test@123');
   await loginBtn.click();
   await page.waitForLoadState('networkidle');
-  const titles= await page.locator('.card-body b').allTextContents();
+  const titles = await page.locator('.card-body b').allTextContents();
   console.log(titles);
   const count = await products.count();
- for (let i = 0; i < count; ++i) {
+  for (let i = 0; i < count; ++i) {
 
-    if (await products.nth(i).locator("b").textContent() === productName)
- 
-  //add cart
-{  await products.nth(i).locator('text= Add To Cart').click();
-  break;
- }
-}
- await page.locator("[routerlink*='cart']").click()
- await page.locator("div li").first().waitFor();
+    if ((await products.nth(i).locator("b").textContent()) === productName)
 
- const bool = await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
-expect(bool).toBeTruthy();
+    //add cart
+    {
+      await products.nth(i).locator('text= Add To Cart').click();
+      break;
+    }
+  }
+  await page.locator("[routerlink*='cart']").click()
+  await page.locator("div li").first().waitFor();
+
+  const bool = await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
+  expect(bool).toBeTruthy();
+
+  // Paymentmethod 
+  await page.locator('button:has-text("Checkout")').click();
+  await page.locator('input[value="4542 9931 9292 2293"]').fill("4542 9931 9292 2295");
+
 
   // if we want to wait for specific element use .waitfor() eg;
   // await cardsTitels.first().waitFor();
